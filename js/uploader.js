baf359ee79d2508bb11f4105b8038938f6f797a8
Nikolai Levchenko <nilevforce@icloud.com>
2026-03-08 00:21:43 +0300

    Добавляет в Uploader отображение загруженного изображения в превью редактора

--- js/uploader.js
+++ js/uploader.js
@@ -1,9 +1,32 @@
 import { openEditor } from './editor.js';
+import { showAlertError } from './alerts.js';
+
+const ALLOWED_FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
 const imageUploadInputElement = document.querySelector('.img-upload__input');
+const imageElement = document.querySelector('.img-upload__preview img');
+
+const isAllowedFileType = (fileName) => {
+  return ALLOWED_FILE_TYPES.some((item) => fileName.endsWith(item));
+};
+
+const onImageUploadInputChange = (evt) => {
+  const file = evt.target.files[0];
+  const fileName = file.name.toLowerCase();
+
+  if(!isAllowedFileType(fileName)) {
+    showAlertError('Неподдерживаемый формат изображения.');
+    return;
+  }
+
+  imageElement.src = URL.createObjectURL(file);
+
+  // Добавляет задержку для устрания скачка загрузки изображения
+  setTimeout(() => openEditor(), 200);
+};
 
 const initUploader = () => {
-  imageUploadInputElement.addEventListener('change', openEditor);
+  imageUploadInputElement.addEventListener('change', onImageUploadInputChange);
 };
 
 export {
